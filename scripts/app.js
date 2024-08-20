0c92ebcda410ec5477c9a442fbe3f9f8151d8ca7
MarufsLinux <Muhammedmarufraju@gmail.com>
2024-08-20 17:54:54 +0600

    coupon functionality done

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,42 @@
 console.log("hey i m connected");
+document.getElementById("total-price").innerHTML = 0;
+let userSelected = 0;
 document.getElementById("kbd-wrapper").addEventListener("click", (e) => {
   if (e.target.classList.contains("kbd")) {
+    userSelected += 1;
+    if (userSelected > 4) {
+      alert("You can't select more than 4 seats");
+      return;
+    }
+    if (userSelected === 4) {
+      document.getElementById("coupon-button").disabled = false;
+    }
+
     makeFocuse(e);
-    handleDecrement('total-seat');
-    addforCalculations(e)
+    handleDecrement("total-seat");
+    addToTable(e);
+    handleTotalPrice();
+  }
+});
+
+document.getElementById("coupon-button").addEventListener("click", () => {
+  const coupon = document.getElementById("coupon-input").value;
+  console.log("coupon is ", coupon);
+  const condition1 = coupon.toLowerCase() === "couple20";
+  const condition2 = coupon.toLowerCase() === "new15";
+  if (condition1 || condition2) {
+    let totalPrice = parseInt(document.getElementById("total-price").innerText);
+    if (condition1) {
+      const couponedPrice = totalPrice * 0.8;
+      document.getElementById("grand-total").innerText = couponedPrice;
+    }
+    if (condition2) {
+      const couponedPrice = totalPrice * 0.85;
+      document.getElementById("grand-total").innerText = couponedPrice;
+    }
+    document.getElementById("coupon-input").value = "";
+    document.getElementById("coupon-button").disabled = true;
+  } else {
+    alert("no coupon found");
   }
 });
--- scripts/utilites.js
+++ scripts/utilites.js
@@ -7,7 +7,7 @@ function handleDecrement(element) {
   document.getElementById(element).innerHTML =
     parseInt(totalSeatElement.innerText) - 1;
 }
-function addforCalculations(e) {
+function addToTable(e) {
   const seatNumber = e.target.innerText;
   const tablerow = document.createElement("tr");
   tablerow.innerHTML = `<td>${seatNumber}</td>
@@ -17,12 +17,17 @@ function addforCalculations(e) {
        id="ticket-price"
        class="font-bold text-blue-500"
      >
-       500
+       550
      </span>
      <span class="text-xl font-bold">৳</span>
    </td>`;
 
   document.getElementById("table-body").appendChild(tablerow);
+}
 
-  
+function handleTotalPrice() {
+  const totalPricespan = document.getElementById("total-price");
+  const grandTotal = document.getElementById("grand-total");
+  totalPricespan.innerHTML = parseInt(totalPricespan.innerText) + 550;
+  grandTotal.innerHTML = parseInt(document.getElementById("total-price").innerText);
 }
